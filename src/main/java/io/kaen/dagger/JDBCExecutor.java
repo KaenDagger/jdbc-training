e1a9d43b46c5bb845782005ca227e9cefd520453
Rohan Maity <rmrohan03@gmail.com>
2020-04-30 10:22:57 +0530

    Add ordering & limit

--- src/main/java/io/kaen/dagger/JDBCExecutor.java
+++ src/main/java/io/kaen/dagger/JDBCExecutor.java
@@ -19,6 +19,8 @@ public class JDBCExecutor {
         try{
             Connection connection = dcm.getConnection();
             CustomerDao customerDao = new CustomerDao(connection);
+
+//            Insert data
 //            Customer customer = new Customer();
 //            customer.setFirstName("Rohan");
 //            customer.setLastName("Maity");
@@ -30,9 +32,11 @@ public class JDBCExecutor {
 //            customer.setZipCode("908-9081");
 //            customerDao.create(customer);
 
+//            Find by Id SELECT
 //            Customer customer = customerDao.findById(10000);
 //            System.out.println(customer.getFirstName() +" "+customer.getLastName());
 
+//            Update Data
 //            Customer customer = customerDao.findById(10000);
 //            System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " +
 //                    customer.getEmail());
@@ -41,6 +45,8 @@ public class JDBCExecutor {
 //            System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " +
 //                    customer.getEmail());
 //
+
+//            CRUD complete
 //            Customer customer = new Customer();
 //            customer.setFirstName("John");
 //            customer.setLastName("Adams");
@@ -51,6 +57,7 @@ public class JDBCExecutor {
 //            customer.setPhone("(555) 555-9845");
 //            customer.setZipCode("01234");
 //
+//
 //            Customer dbCustomer = customerDao.create(customer);
 //            System.out.println(dbCustomer.getFirstName());
 //            dbCustomer = customerDao.findById(dbCustomer.getId());
@@ -60,15 +67,18 @@ public class JDBCExecutor {
 //            System.out.println(dbCustomer.getFirstName());
 //            customerDao.delete(dbCustomer.getId());
 
-
+//            Get order details by Join
 //            OrderDAO orderDAO = new OrderDAO(connection);
 //            Order order = orderDAO.findById(1000);
 //            System.out.println(order);
 
+//            Stored Procedure
+//            OrderDao orderDAO = new OrderDao(connection);
+//            List<Order> orders = orderDAO.getOrdersForCustomer(789);
+//            orders.forEach(System.out::println);
 
-            OrderDao orderDAO = new OrderDao(connection);
-            List<Order> orders = orderDAO.getOrdersForCustomer(789);
-            orders.forEach(System.out::println);
+            CustomerDao customerDAO = new CustomerDao(connection);
+            customerDAO.findAllSorted(20).forEach(System.out::println);
         }catch (SQLException e){
             e.printStackTrace();
         }
